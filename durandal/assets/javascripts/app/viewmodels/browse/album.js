a5efae5f6804430ae897f5c9c8a4639f495bae52
martijnboland <martijn@boland.org>
2013-11-27 13:28:32 +0100

    Extract disc no from album track list and display grouped.

--- durandal/assets/javascripts/app/viewmodels/browse/album.js
+++ durandal/assets/javascripts/app/viewmodels/browse/album.js
@@ -5,19 +5,21 @@ define(['services/mopidyservice', 'services/artservice', 'durandal/app', 'lodash
   var ctor = function () {
     this.album = {};
     this.artist = '';
-    this.tracks = [];
+    this.discs = [];
     this.albumImageUrl = defaultAlbumImageUrl;
     self = this;
   };
 
   ctor.prototype.activate = function(uri) {
     mopidyservice.getAlbum(uri).then(function(data) {
-      // data comes as a list of tracks. Extract album and artist(s) from first track.
+
+      // data comes as a list of tracks.
       if (data.length > 0) {
+ 
+        // Extract album and artist(s) from first track.
         var firstTrack = data[0];
         self.album = firstTrack.album;
         self.artist = util.getTrackArtistsAsString(firstTrack);
-        self.tracks = data;
 
         artservice.getAlbumImage(self.album, 'extralarge', function(albumImageUrl, err) {
           if (albumImageUrl !== undefined && albumImageUrl !== '') {
@@ -27,6 +29,20 @@ define(['services/mopidyservice', 'services/artservice', 'durandal/app', 'lodash
           {
             self.albumImageUrl = defaultAlbumImageUrl;
           }
+        });
+
+       // Group album into discs
+        var discNo = 1;
+        var currentTrackNo = 1;
+        var discs = _.groupBy(data, function(track) {
+          if (track.track_no < currentTrackNo) {
+            discNo++;
+          }
+          currentTrackNo = track.track_no;
+          return discNo;
+        });
+        _.forEach(discs, function(disc, index) {
+          self.discs.push({ disc: index, items: disc });
         });
       }
     }, console.error);
