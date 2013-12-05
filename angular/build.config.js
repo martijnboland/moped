5379d1883ff84f1b49a2099d53481498ef3bf900
martijnboland <martijn@boland.org>
2013-12-05 16:41:37 +0100

    Cleaning and fixing Bootstrap 3 compatibility.

--- angular/build.config.js
+++ angular/build.config.js
@@ -63,10 +63,7 @@ module.exports = {
   vendor_files: {
     js: [
       'vendor/angular/angular.js',
-      'vendor/angular-bootstrap/ui-bootstrap-tpls.min.js',
-      'vendor/placeholders/angular-placeholders-0.0.1-SNAPSHOT.min.js',
-      'vendor/angular-ui-router/release/angular-ui-router.js',
-      'vendor/angular-ui-utils/modules/route/route.js'
+      'vendor/placeholders/angular-placeholders-0.0.1-SNAPSHOT.min.js'
     ],
     css: [
     ],
